2cb995adf6a02042cae73b410c0c1b9a226cedfa
DevAman-sudo <devaman.net@gmail.com>
2020-12-13 21:57:52 +0545

    Api Security Update

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+// NPM Imported Packages //
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+// Logging In On Discord Bot //
+const token = process.env.BOT_TOKEN;
+client.login(token);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Handiling Server Messages //
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (msg.content === 'hii') {
         msg.reply('pong');
     }
 });
-
-client.login('Nzg3NjI2NDkzNDI5MDIyNzIw.X9XsVQ.9fMcAkBrhVkI6lHiAbeuZXHAWJA');
